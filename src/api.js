b6248d60677b6f2a3f218c9868f0fed9d7fa3d25
guilherme bandeira <guilhermepb7@outlook.com>
2024-03-20 11:12:49 -0300

    configuring axios

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    //baseURL: "http://172.233.25.218:85"
-    baseURL: "https://localhost:7245"
+    baseURL: "http://172.233.25.218:85"
+    //baseURL: "https://localhost:7245"
 })
 
 export default api;
